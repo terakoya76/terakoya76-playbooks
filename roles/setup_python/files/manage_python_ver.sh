b625b95c644ff5dac78ce0646ed3d3ae3e403779
Hajime Terasawa <hajime-terasawa@c-fo.com>
2018-11-04 23:15:24 +0900

    :sparkles: update a way to manage python version

--- roles/setup_python/files/manage_python_ver.sh
+++ roles/setup_python/files/manage_python_ver.sh
@@ -2,5 +2,5 @@ PY2_VERSION="2.7.15"
 PY3_VERSION="3.6.5"
 
 pyenv global "$PY3_VERSION" "$PY2_VERSION"
-export PATH="$HOME/.pyenv/versions/$PY3_VERSION/bin/:$PATH"
-export PATH="$HOME/.pyenv/versions/$PY2_VERSION/bin/:$PATH"
+export PATH="$HOME/.anyenv/envs/pyenv/shims/python2/:$PATH"
+export PATH="$HOME/.anyenv/envs/pyenv/shims/python3/:$PATH"
