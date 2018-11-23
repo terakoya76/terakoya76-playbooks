857314eb4e18b6f922647ef8d9344235346af42c
Hajime Terasawa <hajime-terasawa@c-fo.com>
2018-11-24 01:11:25 +0900

    :bug: IMPROVE: install direnv before updating dotfiles

--- setup.sh
+++ setup.sh
@@ -7,6 +7,32 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
+# setup ansible
+case "$1" in
+  mac)
+    if ! which brew > /dev/null 2>&1 ;then
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    brew install ansible ;;
+  ubuntu)
+    sudo apt-get update
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install ansible ;;
+esac
+
+# setup direnv
+case "$1" in
+  mac)
+    brew install direnv ;;
+  ubuntu)
+    wget -O direnv https://github.com/direnv/direnv/releases/download/v2.6.0/direnv.linux-amd64
+    chmod +x direnv
+    sudo cp direnv /usr/local/bin/
+    sudo rm direnv ;;
+esac
+
 REPOSITORY_PATH="$HOME/terakoya76-playbooks"
 cd "$REPOSITORY_PATH"
 
