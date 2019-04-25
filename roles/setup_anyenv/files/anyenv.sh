98ddeab7b77de40c7b6e2e730b7b91a4545fc4cb
Hajime Terasawa <hajime-terasawa@c-fo.com>
2019-04-25 18:03:32 +0900

    :sparkles: make all installation  passed

--- roles/setup_anyenv/files/anyenv.sh
+++ roles/setup_anyenv/files/anyenv.sh
@@ -1,2 +1,3 @@
 export PATH="$HOME/.anyenv/bin:$PATH"
+export ANYENV_ROOT="$HOME/.anyenv"
 eval "$(anyenv init -)"
--- roles/setup_python/files/manage_python_ver.sh
+++ roles/setup_python/files/manage_python_ver.sh
@@ -1,5 +1,5 @@
 PY2_VERSION="2.7.15"
-PY3_VERSION="3.6.5"
+PY3_VERSION="3.7.3"
 
 pyenv global "$PY3_VERSION" "$PY2_VERSION"
 export PATH="$HOME/.anyenv/envs/pyenv/shims/python2/:$PATH"
