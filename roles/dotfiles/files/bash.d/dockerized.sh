98bde7a8e09156f67a452c62d262502319670125
Hajime Terasawa <hajime-terasawa@c-fo.com>
2019-09-03 18:26:06 +0900

    :gear: dockerize terraform command

--- roles/dotfiles/files/bash.d/dockerized.sh
+++ roles/dotfiles/files/bash.d/dockerized.sh
@@ -17,3 +17,11 @@ docker run -d -p 27017:27017 mongo
 # redis
 docker pull redis
 docker run -d -p 6379:6379 redis redis-server --appendonly yes
+
+# terraform
+TERRAFORM11=hashicorp/terraform:0.11.14
+TERRAFORM12=hashicorp/terraform:0.12.7
+docker pull $TERRAFORM11
+docker pull $TERRAFORM12
+alias terraform11="docker run -it --rm $TERRAFORM11"
+alias terraform12="docker run -it --rm $TERRAFORM12"
