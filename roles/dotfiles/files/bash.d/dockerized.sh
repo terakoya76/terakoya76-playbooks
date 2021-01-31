993367051e12891e39080e96923073238612129b
terakoya76 <terako.studio@gmail.com>
2021-01-31 16:55:39 +0900

    fix: make init load fast

--- roles/dotfiles/files/bash.d/dockerized.sh
+++ roles/dotfiles/files/bash.d/dockerized.sh
@@ -1,7 +1,3 @@
-#
-# official
-#
-
 # mitmproxy
 alias mitmproxy='docker run -it --rm -v ~/.mitmproxy:/home/mitmproxy/.mitmproxy -p 8080:8080 mitmproxy/mitmproxy'
 
@@ -12,23 +8,7 @@ alias ssm-sh='docker run -it --rm itsdalmo/ssm-sh'
 alias conftest='docker run --rm -v $(pwd):/project instrumenta/conftest'
 
 # mongodb
-docker run -d -p 27017:27017 mongo
+alias mongo='docker run --rm -p 27017:27017 mongo'
 
 # redis
-docker run -d -p 6379:6379 redis redis-server --appendonly yes
-
-# terraform
-TERRAFORM11=hashicorp/terraform:0.11.14
-TERRAFORM12=hashicorp/terraform:0.12.7
-alias terraform11="docker run --rm -v $(pwd):/project $TERRAFORM11"
-alias terraform12="docker run --rm -v $(pwd):/project $TERRAFORM12"
-
-#
-# personal
-#
-
-# terraformer
-alias terraformer='docker run --rm terakoya76/terraformer'
-
-# iam-policy-json-to-terraform
-alias iam-policy-json-to-terraform='docker run --rm terakoya76/iam-policy-json-to-terraform'
+alias redis='docker run --rm -p 6379:6379 redis redis-server --appendonly yes'
